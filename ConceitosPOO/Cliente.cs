c10990ca9b2c4666933324825ecba398dc860d42
UgoCoelho <2024102070@academicougb.com.br>
2026-03-03 19:59:23 -0300

    conceitos de poo, classe cliente com atributos e nome e cpf

---
+++ ConceitosPOO/Cliente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceitosPOO
+{
+    public class Cliente
+    {
+        
+        private string _nome;
+        
+        public string Nome 
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (value.Length > 0)
+                    _nome = value;
+                else
+                    throw new Exception("O nome não pode estar vazio!");
+            }
+        }
+
+        private string _cpf;
+
+        public string Cpf
+        {
+            get
+            {
+                return _cpf;
+            }
+            set
+            {
+                if (value.Length == 11)
+                {
+                    string cpfFormatado = Convert.ToUInt64(value)
+                        .ToString(@"000\.000\.000\-00");
+
+                    Console.WriteLine($"CPF do cliente: {cpfFormatado}");
+                }
+                else
+                {
+                    Console.WriteLine("CPF inválido!");
+                }
+            }
+        }
+    }
+}
---
+++ ConceitosPOO/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceitosPOO;
+
+Console.WriteLine("Hello, World!");
+
+Cliente cliente = new Cliente();
+Console.WriteLine("Digite seu nome: ");
+cliente.Nome = Console.ReadLine();
+Console.WriteLine($"Nome do cliente: {cliente.Nome}");
+
+Console.WriteLine("Digite seu Cpf: ");
+cliente.Cpf = Console.ReadLine();
+Console.WriteLine($"Nome do cliente: {cliente.Cpf}");
